1d162ccfa05f7f4ab06705d57f7f14757414fc7a
lupeMorales <morales.lupe@gmail.com>
2022-12-27 14:32:16 +0100

    add launchScreem

---
+++ YouTubeClone/YouTubeClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YouTubeClone
+//
+//  Created by Guadalupe Morales carmona on 27/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
